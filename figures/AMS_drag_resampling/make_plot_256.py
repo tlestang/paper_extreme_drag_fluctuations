fedc62d66bf7e8d1d32201ca9c31441df8f6f308
Thibault Lestang <thibault.lestang@hlrs.de>
2019-02-06 17:27:11 +0100

    AMS: Adds plots illustrating the saturation of the level and extinction

---
+++ figures/AMS_drag_resampling/make_plot_256.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+# This scripts produces a figure that illustrates how the maximum over the resamplied trajectories
+# saturates. It shows that that the TAMS does not lead to higher fluctuations, with respect to the
+# initial ensemble of trajectories.
+#
+# Original data can be found in
+# transition_hdd/thibault/These/AMS/libTAMS/tests/experience_lbm/courbe_efficacite_instant
+# Original MATLAB script is make_plot_256.m
+
+prefix = '/home/thibault/transition_hdd/thibault/These/AMS/libTAMS/tests/experience_lbm/' \
+        +'data/instant_256_20/'
+
+# Defines average and std of drag process, compute over the control run
+m = 0.0252
+sig = 0.0412
+
+# Defines relevant parameters of the TAMS experiment
+Nc = 256;
+Ta = 20;
+
+# Opens file and read maxima on resampled trajectories
+amax = (np.fromfile(prefix+'amax.dat')-m)/sig
+Nresamp = len(amax)
+
+# Creates a 1D array that contains the computational cost for each resamp. traj.
+# It is over-evaluated as the formula below considers that each resamp. traj. was simulated
+# over the full duration Ta
+Cvec = Nc*Ta*np.ones(Nresamp) + Ta*np.arange(1,Nresamp+1)
+
+# Init. figure
+fig,ax = plt.subplots(figsize=(9,5), constrained_layout=True)
+
+# Gathers the maxima over the initial ensemble
+maxArray = np.zeros(Nc)
+for j in range(0,Nc):
+    f = np.fromfile(prefix+'traj_{:d}_costFunc_iter_0.dat'.format(j))
+    maxArray[j]=(np.amax(f)-m)/sig
+# Plots a line that represents the level of the highest fluctuation in
+# the initial ensemble
+M = np.amax(maxArray)
+line, = ax.plot([Cvec[40], Cvec[Nresamp-1]], [M, M], '--')
+# Plots the maxima for Cvec = Nc*Ta (initial computational cost)
+init, = ax.plot(Nc*Ta*np.ones(Nc), maxArray, '+', linestyle='None')
+init.set_markersize(5)
+init.set_color(line.get_color())
+init.set_label('Maxima over initial trajectories')
+
+# Plots maxima over resampled trajectories
+resamp, =ax.plot(Cvec, amax, linestyle='None')
+resamp.set_marker('o')
+resamp.set_markersize(3)
+resamp.set_markerfacecolor('#6666ff');
+resamp.set_markeredgecolor(resamp.get_markerfacecolor())
+resamp.set_label('Maxima over resampled trajectories')
+
+ax.set_xscale('log')
+ax.set_xlabel(r'$C_{TAMS} / \tau_c$',fontsize=16)
+ax.set_ylabel(r'$f_d^{\star} / \sigma$',fontsize=18)
+ax.tick_params(axis="x", labelsize=16)
+ax.tick_params(axis="y", labelsize=16)
+
+ax.legend(loc=7, fontsize=12)
+
+
+plt.show()
---
+++ figures/AMS_drag_trajectories/plot_pre_extinct.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+# This scripts produces a figure that illustrates how the resampled trajectories eventually
+# concentrate on a unique trajectory. 
+#
+# Original data can be found in
+# It shows that that the TAMS does not lead to higher fluctuations, with respect to the
+# initial ensemble of trajectories.
+# /home/thibault/transition_hdd/thibault/These/AMS/libTAMS/tests/experience_lbm/plots_instant_32_5/
+# Original MATLAB script is plot_pre_extinct.m
+
+prefix = '/home/thibault/transition_hdd/thibault/These/AMS/libTAMS/tests/experience_lbm/' \
+        +'data/instant_32_5/'
+
+# Defines average and std of drag process, compute over the control run
+m = 0.0252
+sig = 0.0412
+
+# Defines relevant parameters of the TAMS experiment
+Nc = 32;
+Ta = 5;
+
+tspan = np.linspace(0,5,5000)
+maxArray=np.zeros(Nc)
+# Init. figure
+fig,ax = plt.subplots(figsize=(9,5), constrained_layout=True)
+
+for i in range(0,Nc):
+    x = np.fromfile(prefix+'traj_{:d}_costFunc_iter_0.dat'.format(i))
+    x=(x-m)/sig
+    ax.plot(tspan, x, color='#006699',zorder=i+1)
+    maxArray[i] = np.amax(x)
+
+maxIdx = np.argmax(maxArray)
+ax.lines[maxIdx].set_color('#cc0000')
+ax.lines[maxIdx].set_linewidth(1.5)
+ax.lines[maxIdx].set_linestyle('--')
+ax.lines[maxIdx].set_zorder(Nc+1)
+ax.lines[maxIdx].set_label('Initial traj. with the highest maximum')
+print(maxIdx)
+nbKilledTraj = np.fromfile(prefix+'nbKilledTraj.dat', dtype='int32')
+killedTraj = np.fromfile(prefix+'killedTraj.dat', dtype='int32')
+for t in range(1,182):
+    for i in range(0,nbKilledTraj[t-1]):
+        ktIdx = killedTraj[t+i-1]
+        print(ktIdx)
+        print('traj_{:d}_costFunc_iter_{:d}.dat'.format(ktIdx,t))
+        x = np.fromfile(prefix+'traj_{:d}_costFunc_iter_{:d}.dat'.format(ktIdx,t))
+        x=(x-m)/sig
+        ax.lines[ktIdx].set_ydata(x)
+
+ax.set_xlabel(r'$t / \tau_c$',fontsize=16)
+ax.set_ylabel(r'$f_d(t) / \sigma$',fontsize=18)
+ax.tick_params(axis="x", labelsize=16)
+ax.tick_params(axis="y", labelsize=16)
+
+ax.legend(loc='best', fontsize=12)
+ax.set_xlim((0,5))
+
+fname = 'plot_extinction.png'
+plt.savefig(fname)
+
+plt.show()
