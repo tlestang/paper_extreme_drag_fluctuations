19f5240ac4116aad1940e6683a1b46e47c2e6a5e
Thibault Lestang <thibault.lestang@hlrs.de>
2019-02-17 20:37:27 +0100

    Updates plot AMS_OU

--- figures/AMS_OU/make_plot_validation_OU.py
+++ figures/AMS_OU/make_plot_validation_OU.py
@@ -19,20 +19,22 @@ n = len(buf)/2
 avec = buf[0:n]
 rtheo = buf[n:]
 
-fig,ax = plt.subplots(figsize=(6,4),constrained_layout=True)
+fig,ax = plt.subplots(figsize=(8,5),constrained_layout=True)
 #ax.plot(xx,amax/sig,marker='o',markersize=1,linestyle='None');
 lineLvl, = ax.plot(xx,Z/sig,linestyle='-',label='TAMS');
-ax.plot(rtheo,avec/sig, linestyle='-',label='Direct sampling')
+ax.plot(rtheo,avec/sig, linestyle='-',label='Direct sampling (return time)')
 plt.annotate('', xy=(12584,10), xytext=(1.7e19,10), arrowprops=dict(arrowstyle='<->',lw=2,color=lineLvl.get_color()))
-plt.text(1e7, 10.2, r'$C(a) = 10^{-17}r(a), a=10\sigma$', fontsize=12)
+plt.text(1e7, 10.2, r'$C(a) = 10^{-17}r(a), a=10\sigma$', fontsize=18)
 ax.set_xscale('log')
-ax.set_xlabel(r'$r(f_d \geq a), C_{TAMS}(f_d \leq a)$',fontsize=16)
-ax.set_ylabel('$a$',fontsize=16)
+ax.set_xlabel(r'$r(f_d \geq a), C_{TAMS}(f_d \geq a)$',fontsize=22)
+ax.set_ylabel('$a$',fontsize=22)
 ax.set_yticks([0,2,4,6,8,10])
 ax.set_xticks([1e2,1e8,1e14,1e20])
 ax.set_xticklabels((r'$10^2\tau_c$',r'$10^8\tau_c$',r'$10^{14}\tau_c$',r'$10^{20}\tau_c$'))
 ax.set_yticklabels((r'$0$',r'$2\sigma$',r'$4\sigma$',r'$6\sigma$',r'$8\sigma$',r'$10\sigma$'))
-ax.legend(loc='best')
+ax.legend(loc='best', fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
 
 fname = 'AMS_OU.png'
 plt.savefig(fname)
