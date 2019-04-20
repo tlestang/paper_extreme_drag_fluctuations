6b20ac33a239cee8488d4b6ffc01ade6e241515c
Thibault Lestang <thibault.lestang@gmail.com>
2019-04-20 18:19:14 +0100

    Fix unit of time in figure typical_drag_signal

--- figures/typical_drag_signal/plot_drag_timeseries.py
+++ figures/typical_drag_signal/plot_drag_timeseries.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
 
-path_to_file = "/home/thibault/transition_hdd/thibault/These/lbm_code/seq/draft/control_run/control_1" \
+path_to_file = "/home/tlestang/transition_hdd/thibault/These/lbm_code/seq/draft/control_run/control_1" \
                + "/data_force.datout"
-tau_c = 1000;
-f = np.fromfile(path_to_file, float, 250*tau_c, "");
-tspan = np.linspace(0,1000,len(f))
+tau_0 = 500; # Turnover time in units of LBM timesteps
+             # The correlation time tau_c is 2000 timesteps, and the
+             # tau_c is 4tau_0
+f = np.fromfile(path_to_file, float, 500*tau_0, "");
+tspan = np.linspace(0,tau_0,len(f))
 
 fig,ax = plt.subplots(1,1,figsize=(8,3), constrained_layout=True)
-ax.set_xlim(0, 1000)
+ax.set_xlim(0, tau_0)
 line, = ax.plot(tspan, f)
 line.set_linewidth(.7)
 
-ax.set_xlabel(r'$t / \tau_c$',fontsize=22)
+ax.set_xlabel(r'$t / \tau_0$',fontsize=22)
 plt.ylabel('$f_d(t)$',fontsize=22)
 ax.set_yticks(np.arange(-0.10,0.30,0.10))
 plt.xticks(fontsize=16)
