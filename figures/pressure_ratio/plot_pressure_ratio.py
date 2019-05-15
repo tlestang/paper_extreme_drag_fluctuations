Fix signs for figure pressure_ratio

figures/pressure_ratio/plot_pressure_ratio.py
@@ -13,7 +13,7 @@ meanDrag = 0.0252;
 M = (meanPf + meanPr)/2;
 sig = 0.0412;
 mu = 0.0252;
-prefix = '/home/thibault/transition_hdd/thibault/These/lbm_code/seq/draft/etude_dynamique/instant_events/';
+prefix = '/home/tlestang/transition_hdd/thibault/These/lbm_code/seq/draft/etude_dynamique/instant_events/';
 maxDrag = np.zeros(NbEvents)
 frontContrib = np.zeros(NbEvents)
 rearContrib = np.zeros(NbEvents)
@@ -38,10 +38,10 @@ maxDrag = (maxDrag-mu) / sig
 fig,ax = plt.subplots(figsize=(9,5), constrained_layout=True)
 
 
-lineRear, =ax.plot(maxDrag, rearContrib, marker='o', label=r'$\tilde{p}^{\star}_{base}$')
+lineRear, =ax.plot(maxDrag, rearContrib, marker='o', label=r'$-\tilde{p}^{\star}_{base}/\tilde{f}^{\star}_d$')
 lineRear.set_linestyle('None')
 
-lineFront, =ax.plot(maxDrag, frontContrib, marker='^', label=r'$\tilde{p}^{\star}_{fb}$')
+lineFront, =ax.plot(maxDrag, frontContrib, marker='^', label=r'$\tilde{p}^{\star}_{fb}/\tilde{f}^{\star}_d$')
 lineFront.set_linestyle('None')
 
 coefFront = np.polyfit(maxDrag, frontContrib, 1);