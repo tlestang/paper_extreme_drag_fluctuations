72a741e9342388e624f16a0d4630969463e7f729
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-01-18 18:09:56 +0000

    Add script to sort GKTL trajectories according to maximum of instant drag

---
+++ figures/illustr_extrms_vorticity_GKTL/sort_extremes.py
@@ -0,0 +1,46 @@
+from os.path import join
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+
+from jfm_paper import utils
+
+description="Sort trajectories sampled by the GKTL algorithm according to the value of the maximum drag."
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("directory", type=str, help="Location of GKTL directory relative to the current directory.")
+
+args = parser.parse_args()
+
+musimga = np.fromfile(
+    "/home/tlestang/articles/jstat_paper/data/musigma_AVG_10.dat", float, -1, ""
+)
+sig=0.0412 # Standard deviation of instant. drag computed over CR
+m=0.0252 # Average instant. drag computed over CR
+
+# Now group together trajectories that overlap over more than 50% of their
+# total duration.
+# In this example the GKTL run consists in 30 steps and the actual trajectories
+# studied are the last 20 steps.
+# So we want to group together the trajectories that share a common ancestor at step 30 - 20/2 = 20
+dict_of_parents = utils.gktl_group_trajectories(args.directory, br_end=29, br_start=20)
+# dict_of_parents is a dict which entries are a list of overlapping trajs.
+# Loops on group of trajectories
+
+traj_idx = [] # Indices of trajectories sorted
+drag_maxima = [] # Container for the average drag
+for counter, common_ancestor_idx in enumerate(dict_of_parents):
+    # for each group, get index of traj with the highest average
+    drag_maxima_ingroup = []
+    group_of_trajs = dict_of_parents[common_ancestor_idx]
+    for jj in group_of_trajs:
+        fileName=join(args.directory,"recon_rep_0_clone_{}.traj".format(jj))
+        # Reads instant. drag
+        drag_maxima_ingroup.append(np.amax(np.fromfile(fileName, float, -1, "")))
+    
+    j = group_of_trajs[np.argmax(drag_maxima_ingroup)] # Pick traj with the largst maximum in group
+    traj_idx.append(j)
+    drag_maxima.append(np.amax(drag_maxima_ingroup))
+
+with open("extremes.csv", "w") as f:
+    for idx in np.argsort(-np.array(drag_maxima)):
+        f.write("{},{}\n".format(traj_idx[idx],drag_maxima[idx]/sig))
