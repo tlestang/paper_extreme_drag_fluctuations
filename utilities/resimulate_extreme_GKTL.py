d36ecf893e4c95a29b27365d302c4616c0832bb8
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-01-18 17:24:09 +0000

    Add argparse and comments to script resimulate_extreme_GKTL.py

--- utilities/resimulate_extreme_GKTL.py
+++ utilities/resimulate_extreme_GKTL.py
@@ -9,43 +9,66 @@
 
 import os
 import numpy as np
+import argparse
 import simulate
 from jfm_paper import utils
 
+description = (
+    "(re)Simulate the flow locally around maximum of trajectory sampled in a GKTL run."
+)
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument(
+    "directory", type=str, help="GKTL directory location relative to current directory."
+)
+parser.add_argument(
+    "traj_index", metavar="j", type=int, help="The index of the trajectory to simulate."
+)
+args = parser.parse_args()
+
+gktl_dir = args.directory
 
 tau_c = 2000
-Dx = 513
-Dy = 129
-traj_index = 0
-gktl_dir = "../resimul_test"
 vis_window = 4 * tau_c
 
 # Read the drag signal
-filename = os.path.join(gktl_dir, "recon_rep_0_clone_{}.traj".format(traj_index))
+filename = os.path.join(gktl_dir, "recon_rep_0_clone_{}.traj".format(args.traj_index))
 drag_signal = np.fromfile(filename, float, -1, "")
 
 gktl_params = utils.get_gktl_parameters(gktl_dir)
 DT = gktl_params["DT"]
 
-# Compute time points
+# Pressure, velocities and drag will be recorded for tvismin <= t <= tvismax
 t_star = np.argmax(drag_signal)
-tvismin = t_star-vis_window//2
-tvismax = t_star+vis_window//2
-if tvismin<0:
-    print("ERROR: tvismin<0")
-if tvismax>gktl_params["Ta"]-1:
-    print("ERROR: tvismax > Ta")
+tvismin = t_star - vis_window // 2  # Min visualisation boundary window
+tvismax = t_star + vis_window // 2  # Max visualisation boundary window
+if tvismin < 0:
+    msg = "Minimum visualisation boundary should not be < 0. Its value was: {}".format(
+        tvismin
+    )
+    raise Exception(msg)
+if tvismax > gktl_params["Ta"] - 1:
+    msg = "Maximum visualisation boundary should not be > {}. Its value was {}:".format(
+        gktl_params["Ta"] - 1, tvismax
+    )
+    raise Exception(msg)
 
+# Compute starting and end GKTL states
 step_min = tvismin // DT
 step_max = tvismax // DT
 
-# Get population file
-history = utils.gktl_reconstruct_trajectories(gktl_dir, br_end=29, br_start=0, rep=0)
+# Compute history for trajectory
+history = utils.gktl_reconstruct_trajectories(gktl_dir, br_end=29, br_start=10, rep=0)
 
+# Now do resimulation
+# Dimensions of the lattice, required to compute the size of a state.
+Dx = 513
+Dy = 129
 for step in range(step_min, step_max + 1):
-    print(step)
-    ancestor_idx = history[traj_index][step]
-    filename = "rep_0_clone_{}.bin".format(ancestor_idx) 
+    ancestor_idx = history[args.traj_index][step]
+    filename = "rep_0_clone_{}.bin".format(ancestor_idx)
+    # Get initial populations from GKTL ouput file
+    # and write it back to disk in a separate file.
+    # This is necessary to feed it into the simulate module
     with open(os.path.join(gktl_dir, filename), "rb") as f:
         np.fromfile(
             f,
@@ -54,6 +77,7 @@ for step in range(step_min, step_max + 1):
             "",
             offset=step * Dx * Dy * 9 * np.dtype(float).itemsize,
         ).tofile("init_state.bin")
+    # Simulate flow for this GKTL step
     tmin = step * DT
     tmax = (step + 1) * DT - 1
     simulate.simulate("init_state.bin", tmin, tmax, tvismin, tvismax)
