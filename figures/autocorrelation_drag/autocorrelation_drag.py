f7db134405dc4f6faafedc0aa34433119f4bb770
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-02-06 22:51:23 +0000

    Update figure autocorrelation

---
+++ figures/autocorrelation_drag/autocorrelation_drag.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from  scipy import integrate
+from os.path import splitext
+
+R = 16 # Size of the obstable in lattice unit
+U = 0.036 # Mean flow velocity in lattice unit 
+dt = U/R
+
+# -------* Autocorrelation function for drag process *---------
+path_autocorr_drag = '../../data/autocorr_drag.csv'
+try:
+    series = pd.read_csv(path_autocorr_drag, comment='#', index_col=0)
+except FileNotFoundError:
+    sys.exit('Error: Could not find find file {}'.format(path_autocorr_drag))
+
+# Compute integral of autocorrelation to estimate
+# autocorrelation time
+# Note that the autocorrelation is already normalized
+# by variance.
+I = integrate.trapz(series.values, axis=0)
+# Make the autocorrelation time the unit of time
+dt = 1/I
+
+C = series.values
+plt.plot(np.arange(0,C.size)*dt, C,lw=2)
+
+plt.xlabel('$\\tau / T_0$', fontsize=18)
+plt.ylabel('$C(\\tau)$', fontsize=18)
+
+fname = splitext(__file__)[0]+'.png'
+plt.savefig(fname)
+
+plt.show()
