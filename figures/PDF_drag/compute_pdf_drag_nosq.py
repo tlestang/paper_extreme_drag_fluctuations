abdf4d73250b62bd7692aa435e15f968abaf9df5
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-02-06 16:32:33 +0100

    Add scripts to compute pdf of drag with and without square

--- figures/PDF_drag/compute_pdf_drag_nosq.py
+++ figures/PDF_drag/compute_pdf_drag_nosq.py
@@ -1,22 +1,20 @@
 import sys
 import datetime
-from os.path import splitext
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
+from jfm_paper import utils
+
 files = [
-    "/home/tlestang/grid/control_long_{:d}/" "data_force.datout".format(i + 1)
-    for i in range(0, 10)
+    "/home/tlestang/CtrlRunner_v0.2/data_CR_NOSQ_11_02_2018/fdrag.dat"
 ]
 
 print("Computing moments...")
-mu = 0.0253
-var = 0.00173
+mu, var = utils.compute_moments(files, dtype=float)
 
-for i in range(10):
-    print("Loading file {}/10".format(i + 1))
+for i, filename in enumerate(files):
+    print("Loading file {}/{}".format(i+1, len(files)))
     x = np.fromfile(files[i], float, -1, "")
     if i == 0:
         xtot = x
@@ -30,12 +28,12 @@ bin_centers = [0.5 * (b[1] + b[0]) for b in zip(bins[:-1], bins[1:])]
 
 series = pd.Series(data=hist, index=bin_centers)
 
-output_csv = "../../data/PDF_drag.csv"
+output_csv = "../../data/PDF_drag_nosq.csv"
 with open(output_csv, "w") as f:
     f.write("# Generated with {}\n".format(sys.argv[0]))
     f.write("# {}\n".format(str(datetime.datetime.now())))
     f.write("# mu = 0 var = 1")
-    f.write("# /home/tlestang/grid/control_long_{}/data_force.datout")
-    f.write("# using {} files\n".format(10))
+    f.write("# {}".format(files[0]))
+    f.write("# using {} files\n".format(len(files)))
     f.write("#\n")
     series.to_csv(f, header=True)
