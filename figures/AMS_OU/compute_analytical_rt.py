53700d63c83b8c7033b230f720fac73a76ffdc48
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-04-03 09:27:25 +0100

    AMS_OU: Add script to compute analytical solution for return times

---
+++ figures/AMS_OU/compute_analytical_rt.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+
+def g(z):
+    return np.exp(z*z)
+def f0(y):
+    I = integrate.quad(lambda z: np.exp(-z*z),-np.inf,y)
+    return np.exp(y*y)*I
+def fstat(y):
+    I = integrate.quad(lambda z: np.exp(-z*z),-np.inf,y)[0]
+    return np.exp(y*y)*I*I
+sig = 1./np.sqrt(2.)
+avec = np.linspace(sig,10.5*sig,128);
+oneOvEps = 2.
+prefact = (oneOvEps*oneOvEps*oneOvEps) / (2.*np.pi)
+
+k = 0;
+#r0 = np.zeros(len(aVec))
+rstat = np.zeros(len(avec))
+for a in avec:
+#    r0[k] = oneOvEps * integrate.quad(f0,0,a)
+    rstat[k] = np.sqrt(prefact) * integrate.quad(fstat,-20,a)[0]
+    k = k+1;
+
+fo = open("return_time_analytical.dat", "wb")
+avec.tofile(fo, "")
+rstat.tofile(fo, "")
+fo.close()
+    
+  
