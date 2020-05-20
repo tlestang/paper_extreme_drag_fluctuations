76cf8f25adef052fefd5bfe127d7f3c7d03f4437
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-05-20 11:51:47 +0100

    Remove reference to TAMS in figure 22

--- figures/fig22/fig22.py
+++ figures/fig22/fig22.py
@@ -26,12 +26,12 @@ rtheo = buf[n:]
 
 fig,ax = plt.subplots(figsize=(8,5),constrained_layout=True)
 #ax.plot(xx,amax/sig,marker='o',markersize=1,linestyle='None');
-lineLvl, = ax.plot(xx,Z/sig,linestyle='-',label='TAMS');
+lineLvl, = ax.plot(xx,Z/sig,linestyle='-',label='AMS');
 ax.plot(rtheo,avec/sig, linestyle='-',label='Direct sampling (return time)')
 plt.annotate('', xy=(12584,10), xytext=(1.7e19,10), arrowprops=dict(arrowstyle='<->',lw=2,color=lineLvl.get_color()))
 plt.text(1e7, 10.2, r'$C(a) = 10^{-17}r(a), a=10\sigma$', fontsize=18)
 ax.set_xscale('log')
-ax.set_xlabel(r'$r(x \geq a), C_{TAMS}(x \geq a)$',fontsize=22)
+ax.set_xlabel(r'$r(x \geq a), C_{AMS}(x \geq a)$',fontsize=22)
 ax.set_ylabel('$a$',fontsize=22)
 ax.set_yticks([0,2,4,6,8,10])
 ax.set_xticks([1e2,1e8,1e14,1e20])
